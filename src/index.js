87eeaab78172062b61ad7fbbf967a58d5d04895d
juliaschnell <julia.h.schnell@gmail.com>
2021-03-13 10:23:14 -0800

    added circles in SF

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import custom from "./custom-style.json";
 
 let map;
 
-function init() {
+async function init() {
+    const sites = await import("../data/sites.json")
     const style = map.getStyle();
 
     style.sources = {
@@ -15,6 +16,8 @@ function init() {
     };
     style.layers.push(...custom.layers);
     map.setStyle(style);
+
+    map.getSource("sites").setData(sites);
 }
 
 mapboxgl.accessToken = settings.accessToken;
